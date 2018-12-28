099db3bf5dcdefabbc2c1371a3b41e518da54946
ladyada <limor@ladyada.net>
2018-12-28 14:37:31 -0500

    update for feather m0/m4

--- examples/esp8266_tft_featherwing.py
+++ examples/esp8266_tft_featherwing.py
@@ -1,39 +1,43 @@
-# Quick test of TFT FeatherWing (ILI9341) with ESP8266 Adafruit MicroPython.
+# Quick test of TFT FeatherWing (ILI9341) with Feather M0 or M4
 # Will fill the TFT black and put a red pixel in the center, wait 2 seconds,
 # then fill the screen blue (with no pixel), wait 2 seconds, and repeat.
 import time
-
+import random
 import busio
 import digitalio
-from board import SCK, MOSI, MISO, GPIO0, GPIO15
+import board
 
-from adafruit_rgb_display import color565
+from adafruit_rgb_display.rgb import color565
 import adafruit_rgb_display.ili9341 as ili9341
 
 
-# Configuratoin for CS and DC pins (these are FeatherWing defaults on ESP8266):
-CS_PIN = GPIO0
-DC_PIN = GPIO15
-# Config for display baudrate (default is 32mhz, about as fast as the ESP supports):
-BAUDRATE = 32000000
+# Configuratoin for CS and DC pins (these are FeatherWing defaults on M0/M4):
+cs_pin = digitalio.DigitalInOut(board.D9)
+dc_pin = digitalio.DigitalInOut(board.D10)
 
+# Config for display baudrate (default max is 24mhz):
+BAUDRATE = 24000000
 
 # Setup SPI bus using hardware SPI:
-spi = busio.SPI(clock=SCK, MOSI=MOSI, MISO=MISO)
+spi = busio.SPI(clock=board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 
 # Create the ILI9341 display:
-display = ili9341.ILI9341(spi, cs=digitalio.DigitalInOut(CS_PIN),
-                          dc=digitalio.DigitalInOut(DC_PIN), baudrate=BAUDRATE)
+display = ili9341.ILI9341(spi, cs=cs_pin, dc=dc_pin, baudrate=BAUDRATE)
 
 # Main loop:
 while True:
+    # Fill the screen red, green, blue, then black:
+    for color in ((255, 0, 0), (0, 255, 0), (0, 0, 255)):
+        display.fill(color565(color))
     # Clear the display
     display.fill(0)
     # Draw a red pixel in the center.
-    display.pixel(120, 160, color565(255, 0, 0))
+    display.pixel(display.width//2, display.height//2, color565(255, 0, 0))
     # Pause 2 seconds.
     time.sleep(2)
-    # Clear the screen blue.
-    display.fill(color565(0, 0, 255))
+    # Clear the screen a random color
+    display.fill(color565(random.randint(0, 255),
+                          random.randint(0, 255),
+                          random.randint(0, 255)))
     # Pause 2 seconds.
     time.sleep(2)
