404f4e3eb42a20927b2fa292689f4dd41079f943
Melissa LeBlanc-Williams <melissa@Melissas-MacBook-Pro.local>
2019-05-06 13:37:15 -0400

    Added simpletest

---
+++ examples/rgbdisplay_simpletest.py
@@ -0,0 +1,46 @@
+# Quick test of TFT FeatherWing (ST7789) with Feather M0 or M4
+# This will work even on a device running displayio
+# Will fill the TFT black and put a red pixel in the center, wait 2 seconds,
+# then fill the screen blue (with no pixel), wait 2 seconds, and repeat.
+import time
+import random
+import digitalio
+import board
+import displayio
+
+from adafruit_rgb_display.rgb import color565
+import adafruit_rgb_display.st7789 as st7789
+
+displayio.release_displays()
+
+# Configuratoin for CS and DC pins (these are FeatherWing defaults on M0/M4):
+cs_pin = digitalio.DigitalInOut(board.D5)
+dc_pin = digitalio.DigitalInOut(board.D6)
+reset_pin = digitalio.DigitalInOut(board.D9)
+
+# Config for display baudrate (default max is 24mhz):
+BAUDRATE = 24000000
+
+# Setup SPI bus using hardware SPI:
+spi = board.SPI()
+
+# Create the ST7789 display:
+display = st7789.ST7789(spi, cs=cs_pin, dc=dc_pin, rst=reset_pin, baudrate=BAUDRATE)
+
+# Main loop:
+while True:
+    # Fill the screen red, green, blue, then black:
+    for color in ((255, 0, 0), (0, 255, 0), (0, 0, 255)):
+        display.fill(color565(color))
+    # Clear the display
+    display.fill(0)
+    # Draw a red pixel in the center.
+    display.pixel(display.width//2, display.height//2, color565(255, 0, 0))
+    # Pause 2 seconds.
+    time.sleep(2)
+    # Clear the screen a random color
+    display.fill(color565(random.randint(0, 255),
+                          random.randint(0, 255),
+                          random.randint(0, 255)))
+    # Pause 2 seconds.
+    time.sleep(2)
