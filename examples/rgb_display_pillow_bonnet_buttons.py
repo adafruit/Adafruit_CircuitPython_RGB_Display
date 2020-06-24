85a445d17d2fcc3502a4860db4e9faaf39f8fee3
Melissa LeBlanc-Williams <melissa@adafruit.com>
2020-06-24 14:28:50 -0700

    Updated import order

--- examples/rgb_display_pillow_bonnet_buttons.py
+++ examples/rgb_display_pillow_bonnet_buttons.py
@@ -27,9 +27,9 @@
 import time
 import random
 from colorsys import hsv_to_rgb
-from PIL import Image, ImageDraw, ImageFont
 import board
 from digitalio import DigitalInOut, Direction
+from PIL import Image, ImageDraw, ImageFont
 import adafruit_rgb_display.st7789 as st7789
 
 # Create the display
